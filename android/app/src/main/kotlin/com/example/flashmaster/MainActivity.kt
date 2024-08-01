278f015b5a093584c057c1d46787abc6971ac01a
Gul e Zahra <gulezahrasyeda980@gmail.com>
2024-08-01 10:47:06 +0500

    Flash Takl

---
+++ android/app/src/main/kotlin/com/example/flashmaster/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flashmaster
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/welcome_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+      options: FirebaseOptions(
+    apiKey: "AIzaSyAn0YVQZzLcA-FI7031pjRhngSwhN8aqwQ",
+    appId: "1:515556768820:android:f083eaa333e26ddbf4de61",
+    messagingSenderId: "515556768820",
+    projectId: "flashmaster-aafc0",
+  ));
+  runApp(FlashChat());
+}
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+
+            // bodyText1: TextStyle(color: Colors.black54),
+            // body1: TextStyle(color: Colors.black54),
+            ),
+      ),
+      initialRoute: WelcomeScreen.id,
+      routes: {
+        WelcomeScreen.id: (context) => WelcomeScreen(),
+        LoginScreen.id: (context) => LoginScreen(),
+        RegistrationScreen.id: (context) => RegistrationScreen(),
+        ChatScreen.id: (context) => ChatScreen(),
+      },
+    );
+  }
+}
